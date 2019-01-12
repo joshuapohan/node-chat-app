907ab3b96f03e923a079968479a5e4595758cc48
senel34 <joshuapohan93@gmail.com>
2019-01-12 09:41:06 +0700

    add styling to chat app

--- public/js/index.js
+++ public/js/index.js
@@ -38,11 +38,14 @@ socket.on('newLocationMessage', function(message){
 jQuery('#message-form').on('submit', function(e){
 	e.preventDefault();
 
+	var messageTextbox = jQuery('[name=message]');
+
 	socket.emit('createMessage', {
 		from:'User',
-		text:jQuery('[name=message').val()
+		text:messageTextbox.val()
 	}, function(){
-
+		messageTextbox.val('');
+		messageTextbox.focus();
 	});
 });
 
@@ -53,12 +56,17 @@ locationButton.on('click', function(){
 		return alert('Geolocation not supported by your browser');
 	}
 
+	locationButton.attr('disabled', 'disabled').text('Sending Location');
+
 	navigator.geolocation.getCurrentPosition(function(position){
+
+		locationButton.removeAttr('disabled').text('Send Location');
 		socket.emit('createLocationMessage', {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
 		});
 	}, function(){
+		locationButton.removeAttr('disabled').text('Send Location');
 		alert('Unable to fetch location');
 	});
 });
