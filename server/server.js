6f2a8de35bf9828f61d3619bad295646146de8a4
senel34 <joshuapohan93@gmail.com>
2019-01-14 21:05:52 +0700

    only send message to same room

--- server/server.js
+++ server/server.js
@@ -19,7 +19,6 @@ var users = new Users();
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) =>{
- console.log('New user connected');
 
  socket.on('join', (params, callback) => {
  	if(!isRealString(params.name) || !isRealString(params.room)){
@@ -37,12 +36,18 @@ io.on('connection', (socket) =>{
  });
 
  socket.on('createMessage', (message, callback) => {
- 	io.emit('newMessage', generateMessage(message.from, message.text));
+	var user = users.getUser(socket.id);
+	if(user && isRealString(message.text)){
+	 	io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+	}
  	callback();
  });
 
  socket.on('createLocationMessage', (coords, callback) => {
- 	io.emit('newLocationMessage', generateLocationMessage('admin', coords.latitude, coords.longitude));
+ 	var user = users.getUser(socket.id);
+	if(user){
+		io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+	}
  });
 
  socket.on('disconnect', function(){
